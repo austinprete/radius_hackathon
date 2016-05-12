c7b540f84deec07f81f13ca36b30a1dd22de9d2e
Austin Prete <austinprete23@gmail.com>
2016-05-12 14:17:16 -0700

    New DB model

--- model.py
+++ model.py
@@ -1,3 +1,4 @@
+# coding=utf-8
 import os
 
 from flask_sqlalchemy import SQLAlchemy
@@ -8,14 +9,16 @@ db = SQLAlchemy()
 class BomboraRecord(db.Model):
     """Bombora Record"""
 
-    __tablename__ = "bombora_records"
+    __tablename__ = "category_stats"
 
     id = db.Column(db.Integer, primary_key=True, autoincrement=True)
+    industry = db.Column(db.TEXT, nullable=False)
+    category = db.Column(db.TEXT, nullable=False)
+    count = db.Column(db.INTEGER, nullable=False)
+    average_score = db.Column(db.DECIMAL, nullable=False)
     date = db.Column(db.DateTime, nullable=False)
-    json_data = db.Column()
 
-
-    def __repr__ (self):
+    def __repr__(self):
         """Statement when printed."""
 
         return "<Bombora JSON Record ID: %s>" % self.id
@@ -27,15 +30,16 @@ def connect_to_db(app):
     """Connect the database to our Flask app."""
 
     DATABASE_URL = os.environ.get("DATABASE_URL",
-                              "postgresql://localhost/rad-insights")
+                                  "postgresql://localhost/rad-insights")
     app.config['SQLALCHEMY_DATABASE_URI'] = DATABASE_URL
     db.app = app
     db.init_app(app)
 
 
 if __name__ == "__main__":
-# allows working with the database directly
+    # allows working with the database directly
 
     from server import app
+
     connect_to_db(app)
     print "Connected to DB."
